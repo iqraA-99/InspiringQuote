d2c8a7271df2589add3a2a39a874c65db6d1c756
iqraA-99 <iqra.ha99@gmail.com>
2025-02-13 18:32:39 +0000

    worked on witch animation and html css updates

--- src/script.js
+++ src/script.js
@@ -1,8 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
   const witch = document.getElementById("witch");
-  setTimeout(() => {
-    witch.style.display = "none"; // Hide after animation
-  }, 3000);
+  const title = document.getElementById("quote");
+
+  if (witch && title) {
+    let position = 0;
+    const stopPosition = title.offsetWidth - 270;
+
+    function moveWitch() {
+      if (position < stopPosition) {
+        position += 5;
+        witch.style.transform = `translateX(${position}px)`;
+        requestAnimationFrame(moveWitch);
+      }
+    }
+
+    moveWitch();
+  }
 });
 
 document.getElementById("dream-btn").addEventListener("click", function () {
